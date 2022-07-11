18b1155deb26df2bbf898c2de84cc5942cb04c3f
mouli7625 <moulibhanagar07@gmail.com>
2022-07-11 11:53:25 +0530

    commit-m"program2"

---
+++ program2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class program2
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        HashMap<Integer, String> map=new HashMap<Integer,String>();
+        map.put(1,"Mango");
+        map.put(2,"Orange");
+        map.put(3,"Apple");
+        map.put(4,"Grapes");
+        System.out.println("Size of the hash map: "+map.size());
+
+    }
+
+}
