f6e0b8cc0abe8da17b1b2bad8461dbee4b99de5b
mouli7625 <moulibhanagar07@gmail.com>
2022-07-11 20:00:45 +0530

    commit -m"program9"

---
+++ program9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class program9 {
+    public static void main(String[]args)
+    {
+        HashMap<Integer, String> map=new HashMap<Integer,String>();
+        map.put(1,"a");
+        map.put(2,"b");
+       System.out.println(map.entrySet());
+
+
+    }
+    
+}
