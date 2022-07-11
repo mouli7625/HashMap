116eacab4a44591e576c92b39564e2ed7bf2d4ae
mouli7625 <moulibhanagar07@gmail.com>
2022-07-11 20:14:18 +0530

    commit -m"program12"

---
+++ program12.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class program12 {
+    public static void main(String[]args)
+    {
+        HashMap<Integer, String> map=new HashMap<Integer,String>();
+        map.put(1,"a");
+        map.put(2,"b");
+       System.out.println(map.values());
+    }
+    
+}
